aa85fa67c3317893dd778fbd8c998d11cd077eef
vvo <vincent.voyer@algolia.com>
2017-04-24 10:43:41 +0200

    Revert "chore(ci): try to fix travis"
    This reverts commit 7c4d1f5626d9ddecd135cdadb5ad0fbb69895fb5.

--- test/ci.sh
+++ test/ci.sh
@@ -5,7 +5,7 @@ set -e # exit when error, no verbose
 if [ "$TEST_SUITE" == "unit" ]; then
   ./node_modules/karma/bin/karma start --single-run
 elif [ "$TRAVIS_SECURE_ENV_VARS" == "true" -a "$TEST_SUITE" == "integration" ]; then
-  http-server . -p 8888 &
+  static -p 8888 &
   sleep 3
   ./node_modules/mocha/bin/mocha --harmony -R spec ./test/integration/test.js
 else
